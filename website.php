5305fc4a26d6b9e4cd3825687dbc00242ac45a33
Jacob Coates <63732720+Jacob-Coates@users.noreply.github.com>
2021-02-27 22:07:40 +0000

    Update website.php

--- website.php
+++ website.php
@@ -1,3 +1,5 @@
-<?php
-	header("Location: https://www.youtube.com/watch?v=iik25wqIuFo);
-?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to https://example.com/</title>
+<meta http-equiv="refresh" content="0; URL=https://example.com/">
+<link rel="canonical" href="https://example.com/">
